bf400df50a24cdd00c015c18efae348b7d439763
Arjun Giri <giriarjun1133@gmail.com>
2022-07-08 23:02:17 +0545

    Updated Settings

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'cloudinary_storage',
     'cloudinary',
 
@@ -155,5 +156,6 @@ CLOUDINARY_STORAGE = {
     'API_KEY': config('API_KEY'),
     'API_SECRET': config('API_SECRET')
 }
+
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
