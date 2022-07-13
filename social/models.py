11f19c2136a66391e9679b0508bf2dc7a2a84a44
Arjun Giri <giriarjun1133@gmail.com>
2022-07-13 13:53:39 +0545

    Updated UserProfiles

--- social/models.py
+++ social/models.py
@@ -37,7 +37,7 @@ class userProfile(models.Model):
 	bio = models.TextField(max_length=200, blank=True, null=True)
 	dob = models.DateField(null=True, blank=True)
 	location = models.CharField(max_length=100, blank=True, null=True)
-	picture = models.ImageField(upload_to='uploads/profile_pictures', default='uploads/profile_pictures/default.jpg', blank=True)
+	picture = models.ImageField(upload_to='uploads/profile_pictures', blank=True)
 	followers = models.ManyToManyField(User, blank=True, related_name='followers')
 
 	def __str__(self):
